f886b757094bb267ffc7ffa1ef04639e78973e00
Mountainbeach <107517998+Mountainbeach@users.noreply.github.com>
2022-06-17 17:39:00 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const advice_quote = document.querySelector(".advice-quote");
+const advice_id = document.querySelector(".advice-id");
+
+
+async function generateAdvice(){
+    var response = await fetch("https://api.adviceslip.com/advice");
+    var data = await response.json();
+    console.log(data.slip.id);
+    advice_id.innerHTML = data.slip.id;
+    advice_quote.innerHTML = data.slip.advice;
+    return
+}
+
